75158646339159e69112825087f7d3df438d319d
janikvonrotz <contact@janikvonrotz.ch>
2022-08-24 10:26:03 +0200

    fix allowed cidr check

--- auth_totp_ip_check/models/allowed_cidrs.py
+++ auth_totp_ip_check/models/allowed_cidrs.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 import ipaddress
 from odoo.exceptions import ValidationError
 import logging
@@ -18,4 +18,4 @@ class TOTPCidr(models.Model):
             try:
                 ipaddress.IPv4Network(rec.cidr)
             except:
-                raise ValidationError(_("%s is not a valid cidr.") % (rec.cidr, rec.age_from, rec.age_to))
+                raise ValidationError(_('This is not a valid cidr.'))
