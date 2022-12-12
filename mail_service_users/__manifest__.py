f8b18a4ac4fb58a427f6aec9e0813e35618e93e5
janikvonrotz <contact@janikvonrotz.ch>
2022-12-12 10:54:13 +0100

    [mail_service_users] update settings compute

--- mail_service_users/__manifest__.py
+++ mail_service_users/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Technical",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "license": "AGPL-3",
     "depends": ["base","mail"],
     "data": ["views/base.xml"],
---
+++ mail_service_users/models/res_config_settings.py
@@ -0,0 +1,13 @@
+from odoo import fields, models, api
+
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = 'res.config.settings'
+
+    @api.depends('company_id')
+    def _compute_active_user_count(self):
+        super()._compute_active_user_count()
+        active_user_count = self.env['res.users'].sudo().search_count([('share', '=', False),('service_user', '=', False)])
+        for record in self:
+            record.active_user_count = active_user_count
