02110e759ef39ac2d524a7d066b4dbecc8c67725
janikvonrotz <contact@janikvonrotz.ch>
2023-02-08 09:46:50 +0100

    [mail_service_users] migrate

---
+++ mail_service_users/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Mail Service Users",
+    "summary": """
+        Exclude service users from warranty contract.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Technical",
+    "version": "14.0.1.1.0",
+    "license": "AGPL-3",
+    "depends": ["base", "mail"],
+    "data": ["views/base.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
