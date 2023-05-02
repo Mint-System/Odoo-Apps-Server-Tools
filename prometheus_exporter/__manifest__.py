d7249e2a3b66e0c07aef6f80ba09bb7516c23413
Janik von Rotz <login@janikvonrotz.ch>
2023-05-02 11:58:48 +0200

    [prometheus_exporter] update

--- prometheus_exporter/__manifest__.py
+++ prometheus_exporter/__manifest__.py
@@ -6,9 +6,9 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Technical",
-    "version": "14.0.1.1.1",
+    "version": "14.0.1.2.0",
     "license": "AGPL-3",
-    "depends": ["base", "web"],
+    "depends": ["base", "web", "mail"],
     "data": [
         "security/ir.model.access.csv",
         "data/ir_metric.xml",
