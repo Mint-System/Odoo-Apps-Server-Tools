3ec65e3bd8482b85fb33c7d1e95d7a356e8c2beb
Janik von Rotz <login@janikvonrotz.ch>
2023-05-03 10:22:05 +0200

    [auth_totp_ip_check] migrate

---
+++ auth_totp_ip_check/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Auth TOTP IP Check",
+    "summary": """
+        Disable totp for specific ip networks.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Technical",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["auth_totp"],
+    "data": ["views/auth_totp_cidr.xml", "security/ir.model.access.csv"],
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
---
+++ auth_totp_ip_check/models/allowed_cidrs.py
@@ -0,0 +1,21 @@
+from odoo import models, fields, api, _
+import ipaddress
+from odoo.exceptions import ValidationError
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class TOTPCidr(models.Model):
+    _name = 'auth_totp.cidr'
+    _description = 'Allowed TOTP Cidr'
+
+    user_ids = fields.Many2many('res.users',string='Users', ondelete='restrict')
+    cidr = fields.Char()
+
+    @api.constrains('cidr')
+    def _validate_cidr(self):
+        for rec in self:
+            try:
+                ipaddress.IPv4Network(rec.cidr)
+            except:
+                raise ValidationError(_('This is not a valid cidr.'))
