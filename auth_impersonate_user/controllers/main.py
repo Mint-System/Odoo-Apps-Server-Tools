48cb6eeea31847bd872cceacc0416e89817991cb
Janik von Rotz <login@janikvonrotz.ch>
2023-10-16 10:21:25 +0200

    auth_impersonate_user: support logout

--- auth_impersonate_user/controllers/main.py
+++ auth_impersonate_user/controllers/main.py
@@ -1,18 +1,43 @@
+import logging
+
 from odoo import http
 from odoo.http import request
 from odoo.service import security
+
 from odoo.addons.web.controllers.main import Home
-import logging
+
 _logger = logging.getLogger(__name__)
 
 
 class ImpersonateHome(Home):
-    @http.route('/web/impersonate', type='http', auth='user', sitemap=False)
+    @http.route("/web/impersonate", type="http", auth="user", sitemap=False)
     def impersonate_user(self, **kw):
         uid = request.env.user.id
         if request.env.user.can_impersonate_user:
-            uid = request.session.uid = int(request.params['uid'])
-            request.env['res.users'].clear_caches()
-            request.session.session_token = security.compute_session_token(request.session, request.env)
+            uid = request.session.uid = int(request.params["uid"])
+            request.env["res.users"].clear_caches()
+            request.session.session_token = security.compute_session_token(
+                request.session, request.env
+            )
 
         return http.local_redirect(self._login_redirect(uid))
+
+    @http.route("/web/session/logout", type="http", auth="none")
+    def logout(self, redirect="/web"):
+
+        # Exit impersonation first
+        if request.session.impersonator_uid:
+            # Restore session info
+            request.session.uid = request.session.impersonator_uid
+            request.session.login = request.session.impersonator_login
+            del request.session["impersonator_uid"]
+            del request.session["impersonator_login"]
+            request.env["res.users"].clear_caches()
+            request.session.session_token = security.compute_session_token(
+                request.session, request.env
+            )
+
+            return request.redirect(self._login_redirect(request.session.uid))
+
+        request.session.logout(keep_db=True)
+        return request.redirect(redirect, 303)
