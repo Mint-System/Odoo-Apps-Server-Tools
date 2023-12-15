878ed7f905e07b25ddefa2e622fa44864cbeecac
Janik von Rotz <login@janikvonrotz.ch>
2023-12-15 10:53:26 +0100

    base_vat_required_vies: update

--- base_vat_required_vies/models/res_partner.py
+++ base_vat_required_vies/models/res_partner.py
@@ -12,13 +12,22 @@ class ResPartner(models.Model):
 
     @api.model
     def _run_vat_test(self, vat_number, default_country, partner_is_company=True):
-        super()._run_vat_test(vat_number, default_country, partner_is_company=True)
-        return self.vies_passed
+        res = super()._run_vat_test(vat_number, default_country, partner_is_company=True)
+
+        # Return false if vat check failed for eu country.
+        is_eu_country = self.country_id in self.env.ref('base.europe').country_ids
+        vies_check = self._run_vies_test(self.vat, self.country_id)        
+        if is_eu_country and vies_check is False:
+            return False
+        elif isinstance(vies_check, str) and vies_check.upper() != self.country_id.code:
+            return False
+        else:
+            return res
 
     @api.model
     def _build_vat_error_message(self, country_code, wrong_vat, record_label):
         return "\n" + _(
-            "The VAT number [%(wrong_vat)s] for [%(country_code)s] did not pass the VIES check."
+            "The VAT number [%(wrong_vat)s] for [%(country_code)s] did not pass the VAT/VIES check."
             "\nNote: the expected format is %(expected_format)s",
             wrong_vat=wrong_vat,
             record_label=record_label,
