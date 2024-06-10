81b9e3efd2855403a0e4d0fcd58400599d40f5cf
Janik von Rotz <login@janikvonrotz.ch>
2024-06-10 11:18:33 +0200

    fix: check subtype

--- mail_tracking_helpdesk_bounce_ticket/models/mail_message.py
+++ mail_tracking_helpdesk_bounce_ticket/models/mail_message.py
@@ -23,29 +23,30 @@ class MailMessage(models.Model):
     def _compute_is_bounced_message(self):
         """Compute 'is_bounced_message' field for the active user"""
         bounced_states = self.get_bounced_states()
+        subtype_id = self.env.ref("mail.mt_comment")
+
         for message in self:
             has_bounced_trackings = bounced_states.intersection(
                 message.mapped("mail_tracking_ids.state")
             )
             message.is_bounced_message = has_bounced_trackings
-
+            
             if (
                 message.is_bounced_message
                 and message.message_type == "comment"
-                and message.subtype_id.name == "Discussions"
+                and message.subtype_id == subtype_id
             ):
                 message._create_helpdesk_ticket()
 
     def _create_helpdesk_ticket(self):
         self.ensure_one()
-        mail_link = self._get_html_link()
-        ticket_vals = {
+        ticket_id = self.env["helpdesk.ticket"].create({
             "name": _("Mail Delivery Issue"),
-        }
-        ticket_id = self.env["helpdesk.ticket"].create(ticket_vals)
+            "partner_id": self.partner_ids[0].id if self.partner_ids else False,
+        })
         message_body = _(
             "Email with ID %s has been bounced. This ticket has been created from: %s",
             self.id,
-            mail_link,
+            self._get_html_link(),
         )
         ticket_id.message_post(body=message_body, message_type="notification")
