bd551da112401e8735859485d6cbcd434a974335
Janik von Rotz <login@janikvonrotz.ch>
2025-04-03 16:26:35 +0200

    feat(base_recursion): migrate

---
+++ base_recursion_limit/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Base Recursion Limit",
+    "summary": """
+        Set python recursion limit.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch/",
+    "license": "AGPL-3",
+    "category": "Technical Settings",
+    "version": "17.0.1.0.0",
+    "depends": ["base"],
+    "images": ["images/screen.png"],
+}
---
+++ base_recursion_limit/models/ir_model.py
@@ -0,0 +1,3 @@
+from sys import setrecursionlimit
+
+setrecursionlimit(10000)
