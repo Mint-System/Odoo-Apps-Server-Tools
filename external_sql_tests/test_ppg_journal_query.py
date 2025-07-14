ac508d4acabdb737051611e26b92d10e0605c5ca
Ulrich Kilian <uk@science-and-more.de>
2025-07-14 10:19:16 +0200

    feat(base_external_mssql): modified sql test

--- external_sql_tests/test_ppg_journal_query.py
+++ external_sql_tests/test_ppg_journal_query.py
@@ -194,13 +194,62 @@ def test_ppg_journal_aggregation(mssql_conn, desc, test_rows):
         non_aggregating_result = cursor.fetchall()
         print("non_aggregating_result:", non_aggregating_result)
 
-
+        
+        # sql = f"""
+        #   WITH CTE AS (
+        #         SELECT 
+        #             BzId,
+        #              CASE 
+        #                 WHEN Seriennummer IS NOT NULL AND Seriennummer != '' THEN Seriennummer
+        #                 WHEN Charge IS NOT NULL AND Charge != '' THEN Charge
+        #                 ELSE '__none__'
+        #             END AS SerienOrCharge,
+        #             Belegnummer,
+        #             Suchbegriff,
+        #             Richtung,
+        #             Row_Create_Time,
+        #             Row_Update_Time,
+        #             SUM(Menge) AS MengeErledigt,
+        #             MAX(Komplett) AS MaxKomplett
+        #         FROM PPG_Journal
+        #         {condition1}
+        #         GROUP BY 
+        #             BzId,
+        #             CASE 
+        #                 WHEN Seriennummer IS NOT NULL AND Seriennummer != '' THEN Seriennummer
+        #                 WHEN Charge IS NOT NULL AND Charge != '' THEN Charge
+        #                 ELSE '__none__'
+        #             END,
+        #             Belegnummer,
+        #             Suchbegriff,
+        #             Richtung,
+        #             Row_Create_Time,
+        #             Row_Update_Time
+        #     )
+        #     SELECT c.BzId,
+        #         c.Belegnummer,
+        #         c.Seriennummer,
+        #         c.Charge,
+        #         c.Suchbegriff,
+        #         c.Richtung,
+        #         c.Row_Create_Time,
+        #         c.Row_Update_Time,
+        #         c.MengeErledigt,
+        #         c.MaxKomplett,
+        #         STUFF(
+        #                 (SELECT ', ' + CAST(ID AS VARCHAR)
+        #                 FROM PPG_Journal
+        #                 WHERE BzId = c.BzId
+        #                 FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'),
+        #                 1, 2, ''
+        #         ) AS id_list
+        # """
 
         sql = f"""
-          WITH CTE AS (
-                SELECT 
+            WITH CTE AS (
+                SELECT
                     BzId,
-                     CASE 
+                    CASE
                         WHEN Seriennummer IS NOT NULL AND Seriennummer != '' THEN Seriennummer
                         WHEN Charge IS NOT NULL AND Charge != '' THEN Charge
                         ELSE '__none__'
@@ -214,9 +263,9 @@ def test_ppg_journal_aggregation(mssql_conn, desc, test_rows):
                     MAX(Komplett) AS MaxKomplett
                 FROM PPG_Journal
                 {condition1}
-                GROUP BY 
+                GROUP BY
                     BzId,
-                    CASE 
+                    CASE
                         WHEN Seriennummer IS NOT NULL AND Seriennummer != '' THEN Seriennummer
                         WHEN Charge IS NOT NULL AND Charge != '' THEN Charge
                         ELSE '__none__'
@@ -228,24 +277,27 @@ def test_ppg_journal_aggregation(mssql_conn, desc, test_rows):
                     Row_Update_Time
             )
             SELECT 
-                c.*,
-                STUFF(
-                    (
-                        SELECT ', ' + CAST(ID AS VARCHAR)
-                        FROM PPG_Journal j
-                        WHERE 
-                            j.BzId = c.BzId
-                            AND ISNULL(NULLIF(COALESCE(j.Seriennummer, j.Charge), ''), '__none__') = c.SerienOrCharge
-                            AND j.Belegnummer = c.Belegnummer
-                            AND j.Suchbegriff = c.Suchbegriff
-                            AND j.Richtung = c.Richtung
-                            AND j.Row_Create_Time = c.Row_Create_Time
-                            AND j.Row_Update_Time = c.Row_Update_Time
-                        FOR XML PATH(''), TYPE
-                    ).value('.', 'NVARCHAR(MAX)'),
-                    1, 2, ''
+                c.BzId,
+                c.Belegnummer,
+                c.SerienOrCharge,
+                c.Suchbegriff,
+                c.Richtung,
+                c.Row_Create_Time,
+                c.Row_Update_Time,
+                c.MengeErledigt,
+                c.MaxKomplett,
+                (
+                    SELECT 
+                        STUFF((
+                            SELECT ', ' + CAST(j.ID AS VARCHAR)
+                            FROM PPG_Journal j
+                            WHERE j.BzId = c.BzId
+                            FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)')
+                        , 1, 2, '')
                 ) AS id_list
-            FROM CTE c;
+            FROM CTE c
+
+
         """
 
         #cursor.execute(sql, tuple(bzid_values))
