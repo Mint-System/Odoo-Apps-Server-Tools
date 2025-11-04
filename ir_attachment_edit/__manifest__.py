0b6dbdc8914f3b6bad62c798b776e4a80ce1d862
Janik von Rotz <login@janikvonrotz.ch>
2025-11-04 15:25:14 +0100

    chore: downgrade module version from 18.0 to 15.0

--- ir_attachment_edit/__manifest__.py
+++ ir_attachment_edit/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://www.mint-system.ch",
     "category": "Repository",
     "development_status": "Production/Stable",
-    "version": "18.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["base"],
     "installable": True,
