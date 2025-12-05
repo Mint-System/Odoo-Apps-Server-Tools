9a54aaeb8e46f59312897ea6c3567ade70836a8e
Janik von Rotz <login@janikvonrotz.ch>
2025-12-05 11:31:11 +0100

    refactor(prometheus_exporter): reorder imports in ir_metric.py for consistency

--- prometheus_exporter/models/ir_metric.py
+++ prometheus_exporter/models/ir_metric.py
@@ -2,7 +2,7 @@ import logging
 
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
-from odoo.tools.safe_eval import safe_eval, datetime
+from odoo.tools.safe_eval import datetime, safe_eval
 
 _logger = logging.getLogger(__name__)
 
